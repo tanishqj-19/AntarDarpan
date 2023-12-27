60a8e9776cf0c158aa515cde5033521d2d301c8d
Tanishq Jain <tanishq.jain19@gmail.com>
2023-12-28 01:32:43 +0530

    modified:   components/NewEntryCard.tsx

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,15 +1,22 @@
+'use client'
+
+import { createNewEntry } from "@/utils/api"
+import { useRouter } from "next/navigation"
 
 
 const NewEntryCard = () => {
+  const router = useRouter()
+  const handleOnClick = async () => {
+    const data = await createNewEntry()
 
-  const handleOnClick = () => {
+    router.push(`/journal/${data.id}`)
 
   }
   return (
     <div
         className="flex justify-center max-w-72 items-center cursor-pointer overflow-hidden rounded-full bg-black text-white shadow shadow-gray "
     >
-        <div className="px-4 py-5 sm:p-4  text-center">
+        <div className="px-4 py-5 sm:p-4  text-center" onClick={handleOnClick}>
             <span className="text-xl text-center ">New Entry</span>
         </div>
     </div>
