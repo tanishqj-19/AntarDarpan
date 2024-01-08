aa997dd3d8f580db78356e2176298cfce93dab93
Tanishq Jain <tanishq.jain19@gmail.com>
2024-01-08 23:57:39 +0530

    updated api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -21,7 +21,12 @@ export const updateEntry =async (id, content) => {
 
     const res = await fetch(new Request(createURL(`/api/journal/${id}`), {
         method: 'PATCH', 
-        body: JSON.stringify({ content })
+        body: JSON.stringify({ content  })
     }))
+
+    if(res.ok){
+        const data = await res.json()
+        return data.data;
+    }
     
 }
